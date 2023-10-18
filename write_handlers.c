2deda55dd317489b8e2792d185253291c88f5933
Jayjay422 <111477475+Jayjay422@users.noreply.github.com>
2023-10-18 03:02:46 +0100

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -2,6 +2,7 @@
 
 /************************* WRITE HANDLE *************************/
 /**
+ * Done by Ibrahim and Segun
  * handle_write_char - Prints a string
  * @c: char types.
  * @buffer: Buffer array to handle print
